42e65b32ce26357240021d9f3064d54134b8dd03
Kristen Cabrera <cabrera.kristen@gmail.com>
2020-04-10 21:17:19 -0400

    cursors and tings

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
+      action: false,
       x: '60%', 
       y: '50%',
       containerWidth: 0, 
@@ -16,29 +17,42 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.myInput.current.offsetWidth + ' ' + this.myInput.current.offsetHeight)
     this.setState({ containerWidth: this.myInput.current.offsetWidth, containerHeight: this.myInput.current.offsetHeight });
   }
 
   handleMouseMove = event => {
-    console.log('hello')
     this.setState({
       x: event.clientX,
       y: event.clientY,
     })
   }
 
+  handleClick = event => {
+    this.setState({action: !this.state.action})
+  }
+
+  handleClickDown = event => {
+    if (this.state.action) {
+      this.setState({action: !this.state.action})
+    }
+  }
+  
+
   render() {
-    const {x, y, containerHeight, containerWidth} = this.state;
+    const {x, y, containerHeight, containerWidth, action} = this.state;
     return (
-      <div >
+      <div onMouseDown={this.handleClickDown}>
+        <header>
         <h1>
           cards I've collected from my subway rides in <span className="no-br">New York City</span>
         </h1>
+        <button onClick={this.handleClick}>{action ? 'click the screen to freeze' : 'let’s dance' }</button>
+        </header>
 
-        <button>let's boogy</button>
+
+        
         <a href="https://kristencabrera.com/"><img className="source" src={source} alt="Made by kris10cabrera alongside a collage with an NYC trash bag, the statue of liberty, and a slice of pizza."/></a>
-        <div onMouseMove={this.handleMouseMove} onTouchMove={this.handleMouseMove} className="mouse-area-wrapper">
+        <div onMouseMove={action ? this.handleMouseMove : undefined} onTouchMove={ action ? this.handleMouseMove : undefined} className="mouse-area-wrapper">
         <div className="mouse-area" ref={this.myInput} >
         <img src="bowie_front.png" alt="Limited edition David Bowie MTA card" style={{width: x, height: y}} className="first subway-card" />
         <img src="port_back.png" alt="Back of MTA card" className="second subway-card" style={{width: containerWidth - x, height: containerHeight - y}}  />
