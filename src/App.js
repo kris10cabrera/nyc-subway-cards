47074861b3e95b0e52700a7471969656b6a23761
Kristen Cabrera <cabrera.kristen@gmail.com>
2020-04-11 00:26:59 -0400

    let's make this responsive!

--- src/App.js
+++ src/App.js
@@ -41,24 +41,25 @@ class App extends React.Component {
   render() {
     const {x, y, containerHeight, containerWidth, action} = this.state;
     return (
-      <div onMouseDown={this.handleClickDown}>
+      <div>
         <header>
         <h1>
           cards I've collected from my subway rides in <span className="no-br">New York City</span>
         </h1>
-        <button onClick={this.handleClick}>{action ? 'click the screen to freeze' : 'let’s dance' }</button>
+        <button onClick={this.handleClick} className={action ? 'border' : undefined}>{action ? 'click the screen to freeze' : 'let’s dance' }</button>
         </header>
 
 
         
         <a href="https://kristencabrera.com/"><img className="source" src={source} alt="Made by kris10cabrera alongside a collage with an NYC trash bag, the statue of liberty, and a slice of pizza."/></a>
-        <div onMouseMove={action ? this.handleMouseMove : undefined} onTouchMove={ action ? this.handleMouseMove : undefined} className="mouse-area-wrapper">
+        <div onMouseMove={action ? this.handleMouseMove : undefined}   onMouseDown={this.handleClickDown} onTouchMove={ action ? this.handleMouseMove : undefined} className="mouse-area-wrapper">
         <div className="mouse-area" ref={this.myInput} >
-        <img src="bowie_front.png" alt="Limited edition David Bowie MTA card" style={{width: x, height: y}} className="first subway-card" />
+        
         <img src="port_back.png" alt="Back of MTA card" className="second subway-card" style={{width: containerWidth - x, height: containerHeight - y}}  />
           <img src="metrocard_front.png" alt="Front of MTA card" style={{width: x, height: y}}  className="third subway-card" />
 
           <img src="airport_back.png" alt="Back of MTA card" className="fourth subway-card" style={{width: containerWidth - x, height: containerHeight - y}} />
+          <img src="bowie_front.png" alt="Limited edition David Bowie MTA card" style={{width: x, height: y}} className="first subway-card" />
         </div>
         </div>
 
